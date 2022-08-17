40f1087f6a47534546e52e092424cdda7aaa6fe1
Syarif <syarifmaulana.id@gmail.com>
2022-08-17 14:10:06 +0700

    y

--- src/server.js
+++ src/server.js
@@ -1,23 +1,15 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
  
  
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.28.189',
-    routes: {
-        cors: {
-            origin: ['*'],
-        },
-    },
+    host: 'localhost',
   });
-
-  server.route(routes);
  
  
   await server.start();
-  console.log(`Server running at ${server.info.uri}`);
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
  
  
